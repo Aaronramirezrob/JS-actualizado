51c653e1c54b75f18c59d9e67b4eeeebceb0e0d0
davuboy <germanramirezrob@gmail.com>
2022-07-27 00:51:12 -0500

    Actualización AddEvenListener + mobile-menu

--- HTML/main.js
+++ HTML/main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuBurguer = document.querySelector(".menu");
+const mobileMenu = document.querySelector (".mobile-menu");
 
 menuEmail.addEventListener("click", toggleDesktopmenu);
+menuBurguer.addEventListener("click", toggleMobilemenu);
 
 function toggleDesktopmenu(){
 desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobilemenu() {
+    mobileMenu.classList.toggle("inactive");
 }
